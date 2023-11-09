847563626d9b7a9f932911c21cc02f7d0b183f2c
Priest Home Scholars <reajenbahenting829gmail.com>
2023-11-09 22:01:05 +0800

    slot added in dashboard

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="relative flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
+        <div class="">
+            <slot name="head">
+               
+            </slot>
+           
+        </div>
+       
+            <slot name="content1">
+             
+            </slot>
+            <slot name="content2" >
+             
+            </slot>
+    
+            <slot name="footer">
+              
+            </slot>
+       
+    </div>
+   
+   
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,307 +1,148 @@
-<template>
-  <div>
-      <Head title="Dashboard" />
-
-      <Layout>
-          <template #header>
-              <h2
-                  class="font-semibold text-2xl text-gray-800 leading-tight"
-              ></h2>
-          </template>
-          <h2 class="font-semibold text-2xl leading-tight">
-              Welcome to Your Dashboard, {{ auth.user.name }}
-          </h2>
-
-          <div class="p-4">
-              <h2 class="font-semibold text-2xl leading-tight">
-                  Dashboard Overview
-              </h2>
-              <p class="text-gray-600 mt-2">
-                  Here you can view important information about your
-                  application.
-              </p>
-
-              <!-- Display the count -->
-              <h2
-                  class="font-semibold text-2xl text-center mt-3 leading-tight"
-                  v-if="
-                      $page.props.auth.user.permissions &&
-                      $page.props.auth.user.permissions.includes(
-                          'manage-users'
-                      )
-                  "
-              >
-                  Admin over view
-              </h2>
-              <div
-                  class="mt-4"
-                  v-if="
-                      $page.props.auth.user.permissions &&
-                      $page.props.auth.user.permissions.includes(
-                          'manage-users'
-                      )
-                  "
-              >
-                  <div
-                      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"
-                  >
-                      <div
-                          class="bg-white rounded-lg shadow-lg p-4 flex items-center justify-between"
-                      >
-                          <div>
-                              <h2
-                                  class="text-lg dark:text-black font-semibold"
-                              >
-                                  Admins
-                              </h2>
-                              <p class="text-gray-500">
-                                  Total: {{ adminCount }}
-                              </p>
-                          </div>
-                          <div class="ml-4">
-                              <i
-                                  class="text-3xl text-gray-900 fas fa-user-secret"
-                              ></i>
-                          </div>
-                      </div>
-                      <div
-                          class="bg-white rounded-lg shadow-lg p-4 flex items-center justify-between"
-                      >
-                          <div>
-                              <h2
-                                  class="text-lg dark:text-black font-semibold"
-                              >
-                                  Super Visor
-                              </h2>
-                              <p class="text-gray-500">
-                                  Total: {{ superVisorCount }}
-                              </p>
-                          </div>
-                          <div class="ml-4">
-                              <i
-                                  class="text-3xl text-cyan-700 fas fa-user-nurse"
-                              ></i>
-                          </div>
-                      </div>
-
-                      <div
-                          class="bg-white rounded-lg shadow-lg p-4 flex items-center justify-between"
-                      >
-                          <div>
-                              <h2
-                                  class="text-lg dark:text-black font-semibold"
-                              >
-                                  Clients
-                              </h2>
-                              <p class="text-gray-500">
-                                  Total: {{ clientCount }}
-                              </p>
-                          </div>
-                          <div class="ml-4">
-                              <i
-                                  class="text-3xl text-blue-500 fas fa-user"
-                              ></i>
-                          </div>
-                      </div>
-                      <div
-                          class="bg-white rounded-lg shadow-lg p-4 flex items-center justify-between"
-                      >
-                          <div>
-                              <h2
-                                  class="text-lg dark:text-black font-semibold"
-                              >
-                                  Users
-                              </h2>
-                              <p class="text-gray-500">
-                                  Total: {{ userCount }}
-                              </p>
-                          </div>
-                          <div class="ml-4">
-                              <i
-                                  class="text-3xl text-green-500 fas fa-users"
-                              ></i>
-                          </div>
-                      </div>
-                      <div
-                          class="bg-white rounded-lg shadow-lg p-4 flex items-center justify-between"
-                      >
-                          <div>
-                              <h2
-                                  class="text-lg dark:text-black font-semibold"
-                              >
-                                  Categories
-                              </h2>
-                              <p class="text-gray-500">
-                                  Total: {{ categoryCount }}
-                              </p>
-                          </div>
-                          <div class="ml-4">
-                              <i
-                                  class="text-3xl text-violet-500 fas fa-list-ul"
-                              ></i>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <h2
-                  class="font-semibold text-2xl text-center mt-5 leading-tight"
-                  v-if="
-                      $page.props.auth.user.permissions &&
-                      $page.props.auth.user.permissions.includes(
-                          'manage-cashiers'
-                      )
-                  "
-              >
-                  Super visor over view
-              </h2>
-              <div
-                  class="mt-4"
-                  v-if="
-                      $page.props.auth.user.permissions &&
-                      $page.props.auth.user.permissions.includes(
-                          'manage-cashiers'
-                      )
-                  "
-              >
-                  <div
-                      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"
-                  >
-                      <div
-                          class="bg-white rounded-lg shadow-lg p-4 flex items-center justify-between"
-                      >
-                          <div>
-                              <h2
-                                  class="text-lg dark:text-black font-semibold"
-                              >
-                                  Cashiers
-                              </h2>
-                              <p class="text-gray-500">
-                                  Total: {{ cashierCount }}
-                              </p>
-                          </div>
-                          <div class="ml-4">
-                              <i
-                                  class="text-3xl text-sky-900 fas fa-user-tag"
-                              ></i>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <h2
-                  class="font-semibold text-2xl text-center mt-5 leading-tight"
-                  v-if="
-                      $page.props.auth.user.permissions &&
-                      $page.props.auth.user.permissions.includes(
-                          'manage-sales'
-                      )
-                  "
-              >
-                  Cashier over view
-              </h2>
-              <div
-                  class="mt-4"
-                  v-if="
-                      $page.props.auth.user.permissions &&
-                      $page.props.auth.user.permissions.includes(
-                          'manage-sales'
-                      )
-                  "
-              >
-                  <div
-                      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"
-                  >
-                      <div
-                          class="bg-white rounded-lg shadow-lg p-4 flex items-center justify-between"
-                      >
-                          <div>
-                              <h2
-                                  class="text-lg dark:text-black font-semibold"
-                              >
-                                  Products
-                              </h2>
-                              <p class="text-gray-500">
-                                  Total: {{ productCount }}
-                              </p>
-                          </div>
-                          <div class="ml-4">
-                              <i class="text-3xl text-red-500 fas fa-box"></i>
-                          </div>
-                      </div>
-                      <div
-                          class="bg-white rounded-lg shadow-lg p-4 flex items-center justify-between"
-                      >
-                          <div>
-                              <h2
-                                  class="text-lg dark:text-black font-semibold"
-                              >
-                                  Sold Items
-                              </h2>
-                              <p class="text-gray-500">
-                                  Total: {{ soldItemCount }}
-                              </p>
-                          </div>
-                          <div class="ml-4">
-                              <i class="text-3xl text-red-900 fas fa-tag"></i>
-                          </div>
-                      </div>
-                      <div
-                          class="bg-white rounded-lg shadow-lg p-4 flex items-center justify-between"
-                      >
-                          <div>
-                              <h2
-                                  class="text-lg dark:text-black font-semibold"
-                              >
-                                  Sales
-                              </h2>
-                              <p class="text-gray-500">
-                                  Total: {{ saleCount }}
-                              </p>
-                          </div>
-                          <div class="ml-4">
-                              <i
-                                  class="text-3xl text-yellow-300 fas fa-chart-line"
-                              ></i>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-
-              <!-- Add more content as needed -->
-
-              <!-- Example: Recent Activity -->
-              <div class="mt-8">
-                  <h3 class="text-lg dark:text-white font-semibold">
-                      Recent Activity
-                  </h3>
-                  <ul>
-                      <li class="text-gray-600">
-                          User A updated their profile.
-                      </li>
-                      <li class="text-gray-600">
-                          New support ticket created by User B.
-                      </li>
-                      <!-- Add more activity items here -->
-                  </ul>
-              </div>
-          </div>
-      </Layout>
-  </div>
-</template>
-
 <script setup>
 import Layout from "@/Layouts/Layout.vue";
 import { Head } from "@inertiajs/vue3";
+import Card from '@/Components/Card.vue';
+import { inject } from 'vue';
 
-const prop = defineProps({
-  clientCount: Number,
-  userCount: Number,
-  categoryCount: Number,
-  productCount: Number,
-  saleCount: Number,
-  soldItemCount: Number,
-  cashierCount: Number,
-  adminCount: Number,
-  superVisorCount: Number,
-  auth: Object,
-});
+const themeMode = inject('themeMode');
+ const products = [{
+
+    "id": 2,
+            "title": "iPhone X",
+            "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+            "price": 899,
+            "discountPercentage": 17.94,
+            "rating": 4.44,
+            "stock": 34,
+            "brand": "Apple",
+            "category": "smartphones",
+            "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+
+ },
+ {
+    "id": 5,
+            "title": "Huawei P30",
+            "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+            "price": 499,
+            "discountPercentage": 10.58,
+            "rating": 4.09,
+            "stock": 32,
+            "brand": "Huawei",
+            "category": "smartphones",
+            "thumbnail": "https://i.dummyjson.com/data/products/5/thumbnail.jpg",  
+ },
+ {
+
+    "id": 3,
+            "title": "Samsung Universe 9",
+            "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
+            "price": 1249,
+            "discountPercentage": 15.46,
+            "rating": 4.09,
+            "stock": 36,
+            "brand": "Samsung",
+            "category": "smartphones",
+            "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg"
+
+},
+{
+    "id": 6,
+            "title": "MacBook Pro",
+            "description": "MacBook Pro 2021 with mini-LED display may launch between September, November",
+            "price": 1749,
+            "discountPercentage": 11.02,
+            "rating": 4.57,
+            "stock": 83,
+            "brand": "Apple",
+            "category": "laptops",
+            "thumbnail": "https://i.dummyjson.com/data/products/6/thumbnail.png",  
+},
+{
+    "id": 7,
+            "title": "Samsung Galaxy Book",
+            "description": "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+            "price": 1499,
+            "discountPercentage": 4.15,
+            "rating": 4.25,
+            "stock": 50,
+            "brand": "Samsung",
+            "category": "laptops",
+            "thumbnail": "https://i.dummyjson.com/data/products/7/thumbnail.jpg",
+},
+{
+
+    "id": 4,
+            "title": "OPPOF19",
+            "description": "OPPO F19 is officially announced on April 2021.",
+            "price": 280,
+            "discountPercentage": 17.91,
+            "rating": 4.3,
+            "stock": 123,
+            "brand": "OPPO",
+            "category": "smartphones",
+            "thumbnail": "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+}
+]
+
+
 </script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <Layout>
+        <template #header>
+            <h2
+                class="font-semibold text-2xl text-gray-800 leading-tight"
+            ></h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class=" overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="grid">
+                             <Card v-for="prod in products"> 
+                                <template #head> 
+                                    <h3 class=" text-center text-2xl text-black">{{ prod.title }}</h3>
+                                </template>
+                                <template #content1> 
+                                    <img :src="prod.thumbnail" alt="" class="card-image">
+                                </template>
+                                <template #content2> 
+                                   <div class="p-4 rounded bg-blue-500 text-gray-100">
+                                    {{ prod.description }}
+                                   </div>
+                                </template>
+                                <template #footer> 
+                                    <div class="flex justify-between">
+                                        <h2 class="text-2xl text-blue-500 font-bold">
+                                            PhP {{ prod.price }}
+                                        </h2>
+                                    </div>
+                                    
+                                </template>
+                                
+                            </Card>
+                    </div>
+                </div>
+            </div>
+        </div> 
+
+        
+    </Layout>
+</template>
+
+<style scoped>
+.grid{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 24px 24px;
+}
+
+.card-image{
+    width: 100%;
+    aspect-ratio: 16/9;
+    object-fit: cover;
+}
+</style>
+
